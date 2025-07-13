f05f0d04bb840dccd7458ef7542f6dcad087b948
Varika <vartikaagnihotri31@gmail.com>
2025-07-13 21:19:25 +0530

    Added youtube and twitter links successfully with signup, signin and dashboar pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css';
-import { Button } from  './components/ui/Button'
-
-
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Dashboard } from "./pages/dashboard"
 function App() {
-  
-
-  return (
-    <>
-      <Button variant = "secondary" text = "Share"/>
-      <Button variant = "primary" text = "Add Content" />
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
--- frontend/src/icons/PlusIcon.tsx
+++ frontend/src/icons/PlusIcon.tsx
@@ -14,3 +14,4 @@ export function PlusIcon(props: PlusIconProps) {
     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
   </svg>  
 }
+
